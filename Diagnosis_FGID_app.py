bcd2b136fc50c5ec09fbff9bc5de0f07e724a21a
Elahe Mousavi <el.msvi@gmail.com>
2023-04-22 00:44:08 +0330

    Update Diagnosis_FGID_app.py

--- Diagnosis_FGID_app.py
+++ Diagnosis_FGID_app.py
@@ -58,7 +58,9 @@ if st.button("Download output file"):
 #        st.download_button('Download output file', f, file_name="output.csv")
 
 if st.button("Learn about the clusters"): 
-    st.image("https://github.com/Elmsvi-Git/FGID_Diagnosis/blob/main/Images/Cluster1.png")
+    image = Image.open("https://github.com/Elmsvi-Git/FGID_Diagnosis/blob/main/Images/Cluster1.png")
+    st.image(image, caption='Cluster 15')
+
 
 
 
