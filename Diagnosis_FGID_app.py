4a7a478abb3b35552ae98d92932f847abf19d66e
Elahe Mousavi <el.msvi@gmail.com>
2023-04-21 18:12:19 +0330

    Add files via upload

---
+++ Diagnosis_FGID_app.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 22 16:54:22 2023
+
+@author: ElaheMsvi: el.msvi@gmail.com
+"""
+
+from Modulel_Diagnosis import *
+import streamlit as st
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+def Input_Output():
+    data = st.file_uploader('Upload File', type ={"csv"})
+    if data is not None:
+        df = pd.read_csv(data)
+        # st.write(df)
+        clf = FGID_Diagnosis('model1.pkl')
+        clf.load_and_clean_data(df)
+        
+    pred_test = ""
+    if st.button("Click here to predict"):
+        pred_test = clf.predicted_outputs()
+        st.balloons()
+    st.write(pred_test)
+    st.success('The output is as follows\n (The last cloumns indicate the most probable clusters for each case) ')
+
+if __name__== '__main__':
+    Input_Output()
---
+++ Module_Diagnosis.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 22 08:46:14 2023
+
+@author: ElaheMsvi
+"""
+
+###################################################################################
+# import all libraries needed
+import numpy as np
+import pandas as pd
+import pickle
+class FGID_Diagnosis():
+      
+        def __init__(self, model_file):
+            self.clf = pickle.load(open(model_file,'rb'))
+            self.data = None
+###################################################################################       
+        # take a data file (*.csv) and preprocess it in the same way as in the lectures
+        def load_and_clean_data(self, df):
+            
+            # import the data
+            # df = pd.read_csv(data_file,delimiter=',')
+            # store the data in a new variable for later use
+            self.df_with_predictions = df.copy()
+            self.data = df.copy()
+
+        # a function which outputs the probability of a data point to be 1
+        def predicted_probability(self):
+            if (self.data is not None):  
+                pred = self.clf.predict_proba(self.data)#[:,1]
+                return pred
+###################################################################################        
+        # a function which outputs 0 or 1 based on our model
+        def predicted_output_category(self):
+            if (self.data is not None):
+                pred_outputs = self.clf.predict(self.data)
+                return pred_outputs
+###################################################################################        
+        # predict the outputs and the probabilities and 
+        # add columns with these values at the end of the new data
+        def predicted_outputs(self):
+            if (self.data is not None):
+                pred = self.predicted_probability()
+                most_prob_classes = np.zeros([self.data.shape[0], 4])
+                most_prob_classes[: , [0,2]] = np.argsort(pred , axis = 1)[:,[-1,-2]]+1
+                most_prob_classes[: , [1,3]] = np.round(np.sort(pred, axis = 1)[:,[-1,-2]],4)
+                self.df_with_predictions['Most_prob_Cluster1'] = most_prob_classes[:,0]
+                self.df_with_predictions['Probability1'] = most_prob_classes[:,1]
+                self.df_with_predictions['Most_prob_Cluster2'] = most_prob_classes[:,2]
+                self.df_with_predictions['Probability2'] = most_prob_classes[:,3]
+                return self.df_with_predictions
+
