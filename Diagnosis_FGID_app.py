958d12325b70b9d60c15ca8c5108550ea252ef1e
Elahe Mousavi <el.msvi@gmail.com>
2023-04-21 23:42:15 +0330

    Update Diagnosis_FGID_app.py

--- Diagnosis_FGID_app.py
+++ Diagnosis_FGID_app.py
@@ -18,7 +18,7 @@ import os
 
 with st.sidebar: 
     st.image("https://magazine.jhsph.edu/sites/default/files/styles/feature_image_og_image/public/GutBrain_3200x1600.jpg?itok=RqUR2Y2C")
-    st.title("Auto Diagnosis")
+    st.title("FGID Diagnosis")
     choice = st.radio("Navigation", ["Upload","Profiling","Diagnosis", "Download"])
     st.info("This application helps you to classify patients based on GI symptoms")
 
